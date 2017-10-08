3e6addc2c6fceb4ae727b9aa1741004a6f34495a
Kylie <kyliecalif@gmail.com>
2017-10-07 17:21:13 -0700

    add/delete task functioning, light animation of trash/delete completed. Basic version complete

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,29 +1,16 @@
+// Check Off Specific tasks -see note below
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
-// Above: why ul? you can only add a listener (using jQuery) to an existing element when the code is written /first time run
-// now add a click listener to the entire ul parent, when an li is clicked inside a ul then run this code
 
-
-// below prior code doesn not listen to new li's
-// $("li").click(function(){
-// 	$(this).toggleClass("completed");
-// });
-
-
-// Click on X to delete task  - remove the li that contains the clicked on span
+// // Click on span to delete task  - remove the entire li
 $("ul").on("click", "span", function(event){
-	// $(".X").click(function(event){
-			console.log("span has been clicked");
-			//note: $(this).parent().fadeOut().remove; remove won't wait for fadeOut to finish	
-		// $(this).parent().fadeOut("slow",function(){
-		// 	$(this).remove();
-		// });
-		$(this).parent().remove();
-
-		event.stopPropagation();
-// 	//stop the event from bubbling up and not affect the li click event
+	$(this).parent().fadeOut(1000,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
+
 
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
@@ -35,13 +22,37 @@ $("input[type='text']").keypress(function(event){
 	}
 });
 
-// $(".fa-plus").click(function(e){
-// 	e.preventDefault();
-// 	console.log("you clicked on the plus sign");
-// 	$("input").fadeToggle("slow");
-// });
 
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle();
 });
 
+// ********************Notes******************
+// re: Function check off tasks
+// Above: why ul? you can only add a listener (using jQuery) to an existing element when the code is written /first time run
+// -add a click listener to the entire ul parent, when an li is clicked inside a ul then run this code
+
+
+// code below does not listen to newly created li's
+// $("li").click(function(){
+// 	$(this).toggleClass("completed");
+// });
+
+// ****** re: Function delete, fade, remove task ******
+//     $(this).parent().fadeOut().remove; remove won't wait for fadeOut to finish	
+
+
+// 			event.stopPropagation();
+// 			stop the event from bubbling up and not affect the li click event
+
+
+// ----------Ky Code working but removes only - no fade out. 
+// Click on span to delete task  - remove the entire li
+// $("ul").on("click", "span", function(event){
+// 			console.log("span has been clicked");
+// 		$(this).parent().remove();
+
+// 		event.stopPropagation();
+// 		//stop the event from bubbling up and not affect the li click event
+// });
+// ----------
